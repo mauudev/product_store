cb92b0acdf9290086740c134426b31d4dbaea788
mauudev <mtrigo143@gmail.com>
2024-01-19 01:09:57 -0400

    add product model

---
+++ backend/src/apps/products/domain/models/products.py
@@ -0,0 +1,68 @@
+from __future__ import annotations
+
+from typing import AsyncIterator
+
+from pydantic import BaseModel, ConfigDict
+from sqlalchemy import select
+from sqlalchemy.ext.asyncio import AsyncSession
+from sqlalchemy.orm import Mapped, mapped_column
+
+from src.modules.shared.database import Entity
+
+
+class ProductSchema(BaseModel):
+    id: int
+    product_name: str
+    stock: int
+    product_image: str
+
+    model_config = ConfigDict(from_attributes=True)
+
+
+class Product(Entity):
+    __tablename__ = "products"
+
+    id: Mapped[int] = mapped_column(
+        "id", autoincrement=True, nullable=False, unique=True, primary_key=True
+    )
+    product_name: Mapped[str] = mapped_column("product_name", nullable=False)
+    stock: Mapped[int] = mapped_column("stock", nullable=False)
+    product_image: Mapped[str] = mapped_column("product_image", nullable=False)
+
+    @classmethod
+    async def read_all(cls, session: AsyncSession) -> AsyncIterator[Product]:
+        stmt = select(cls)
+        stream = await session.stream_scalars(stmt.order_by(cls.id))
+        async for row in stream:
+            yield row
+
+    @classmethod
+    async def read_by_id(cls, session: AsyncSession, product_id: int) -> Product | None:
+        stmt = select(cls).where(cls.id == product_id)
+        return await session.scalar(stmt.order_by(cls.id))
+
+    @classmethod
+    async def create(
+        cls, session: AsyncSession, product_name: str, stock: int, product_image: str
+    ) -> Product:
+        product = Product(product_name=product_name, stock=stock, product_image=product_image)
+        session.add(product)
+        await session.flush()
+        await session.commit()
+        new = await cls.read_by_id(session, product.id)
+        if not new:
+            raise RuntimeError()
+        return new
+
+    async def update(
+        self, session: AsyncSession, product_name: str, stock: int, product_image: str
+    ) -> None:
+        self.product_name = product_name
+        self.stock = stock
+        self.product_image = product_image
+        await session.flush()
+
+    @classmethod
+    async def delete(cls, session: AsyncSession, product: Product) -> None:
+        await session.delete(product)
+        await session.flush()
