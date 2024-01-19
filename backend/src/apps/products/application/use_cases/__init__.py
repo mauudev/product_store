22b60802d462562f10391b79b2645ba8e3da8ce3
mauudev <mtrigo143@gmail.com>
2024-01-19 02:33:50 -0400

    add list products use case

--- backend/src/apps/products/application/use_cases/__init__.py
+++ backend/src/apps/products/application/use_cases/__init__.py
@@ -1,3 +1,4 @@
 from .create_product import CreateProduct
 from .search_products import SearchProducts
 from .get_product import GetProduct
+from .list_products import ListProducts
---
+++ backend/src/apps/products/application/use_cases/list_products.py
@@ -0,0 +1,16 @@
+from typing import AsyncIterator
+
+from fastapi import HTTPException
+
+from src.apps.products.domain.models import Product, ProductSchema
+from src.modules.shared.database import AsyncSession
+
+
+class ListProducts:
+    def __init__(self, session: AsyncSession) -> None:
+        self.async_session = session
+
+    async def execute(self) -> AsyncIterator[ProductSchema]:
+        async with self.async_session as session:
+            async for product in Product.read_all(session):
+                yield ProductSchema.model_validate(product)
