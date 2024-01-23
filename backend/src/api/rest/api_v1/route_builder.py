389568087b67e47e195d5827bbd6f143d871e28a
mauudev <mtrigo143@gmail.com>
2024-01-23 02:45:52 -0400

    add buy product use case and endpoint

--- backend/src/api/rest/api_v1/route_builder.py
+++ backend/src/api/rest/api_v1/route_builder.py
@@ -1,8 +1,11 @@
-from fastapi import FastAPI
+from fastapi import Depends, FastAPI
 
 from src.api.rest.api_v1.products.routes import router as r_products
 
 
 def build_routes(app: FastAPI) -> FastAPI:
-    app.include_router(r_products, prefix="/v1")
+    app.include_router(
+        r_products,
+        prefix="/v1",
+    )
     return app
--- backend/src/apps/products/application/use_cases/__init__.py
+++ backend/src/apps/products/application/use_cases/__init__.py
@@ -3,3 +3,4 @@ from .get_product import GetProduct
 from .list_products import ListProducts
 from .update_prod_partials import UpdateProductPartial
 from .update_product import UpdateProduct
+from .buy_product import BuyProduct
