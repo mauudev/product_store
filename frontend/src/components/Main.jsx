d0dcbc5787eca71f719f6722d5ffaffb43e57d98
mauudev <mtrigo143@gmail.com>
2024-01-22 21:37:23 -0400

    integrate socket connection on main component

--- frontend/src/components/Main.jsx
+++ frontend/src/components/Main.jsx
@@ -1,25 +1,18 @@
 import React, { useEffect, useState } from "react";
-import { experimentalStyled as styled } from "@mui/material/styles";
 import Box from "@mui/material/Box";
-import Paper from "@mui/material/Paper";
 import Grid from "@mui/material/Grid";
 import Typography from "@mui/material/Typography";
 import Button from "@mui/material/Button";
 import SearchInput from "./SearchInput";
 import ProductCard from "./ProductCard";
+import { socket } from "../shared/socket";
 import { useSelector, useDispatch } from "react-redux";
 import { fetchProducts } from "../actions/productActions";
 import { v4 as uuidv4 } from "uuid";
 
-const Item = styled(Paper)(({ theme }) => ({
-  backgroundColor: theme.palette.mode === "dark" ? "#1A2027" : "#fff",
-  ...theme.typography.body2,
-  padding: theme.spacing(2),
-  textAlign: "center",
-  color: theme.palette.text.secondary,
-}));
-
 const Main = () => {
+  const [isSocketConnect, setIsSocketConnect] = useState(socket.connected);
+  const [fooEvents, setFooEvents] = useState([]);
   const [page, setPage] = useState(1);
   const [size, setSize] = useState(10);
   const [searchTerm, setSearchTerm] = useState("");
@@ -28,6 +21,28 @@ const Main = () => {
 
   useEffect(() => {
     dispatch(fetchProducts({ page, size, name: searchTerm }));
+
+    function onConnect() {
+      setIsSocketConnect(true);
+    }
+
+    function onDisconnect() {
+      setIsSocketConnect(false);
+    }
+
+    function onUpdateStock(payload) {
+      console.log(`=== PRODUCT STOCK UPDATED ${JSON.stringify(payload)}`);
+    }
+
+    socket.on("connect", onConnect);
+    socket.on("disconnect", onDisconnect);
+    socket.on("updateStock", onUpdateStock);
+
+    return () => {
+      socket.off("connect", onConnect);
+      socket.off("disconnect", onDisconnect);
+      socket.off("updateStock", onUpdateStock);
+    };
   }, [page, searchTerm]);
 
   const handleLoadMore = async () => {
@@ -39,8 +54,9 @@ const Main = () => {
     setSearchTerm(search);
   };
 
-  const handlePurchase = (productId) => {
-    console.log("Comprando", productId);
+  const handlePurchase = (productId, quantity) => {
+    console.log(`Purchasing ${quantity} of product ${productId}`);
+    
   };
 
   const filterRenderProducts = (products, searchTerm) => {
