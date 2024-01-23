83f6cffb40a1cad2a1a216a3c7b6fc933291c2e4
mauudev <mtrigo143@gmail.com>
2024-01-23 02:48:06 -0400

    integrate sockets and cards on main app

--- frontend/src/components/Main.jsx
+++ frontend/src/components/Main.jsx
@@ -1,18 +1,17 @@
-import React, { useEffect, useState } from "react";
+import React, { useEffect, useState, useRef } from "react";
+import { useSelector, useDispatch } from "react-redux";
 import Box from "@mui/material/Box";
 import Grid from "@mui/material/Grid";
 import Typography from "@mui/material/Typography";
 import Button from "@mui/material/Button";
 import SearchInput from "./SearchInput";
 import ProductCard from "./ProductCard";
-import { socket } from "../shared/socket";
-import { useSelector, useDispatch } from "react-redux";
-import { fetchProducts } from "../actions/productActions";
+import { createWebSocketConnection } from "../shared/socket";
+import { fetchProducts, buyProduct } from "../actions/productActions";
 import { v4 as uuidv4 } from "uuid";
 
 const Main = () => {
-  const [isSocketConnect, setIsSocketConnect] = useState(socket.connected);
-  const [fooEvents, setFooEvents] = useState([]);
+  const socketRef = useRef(null);
   const [page, setPage] = useState(1);
   const [size, setSize] = useState(10);
   const [searchTerm, setSearchTerm] = useState("");
@@ -22,27 +21,30 @@ const Main = () => {
   useEffect(() => {
     dispatch(fetchProducts({ page, size, name: searchTerm }));
 
-    function onConnect() {
-      setIsSocketConnect(true);
-    }
+    const handleSocket = async () => {
+      try {
+        const socketInstance = await createWebSocketConnection();
+        socketRef.current = socketInstance;
 
-    function onDisconnect() {
-      setIsSocketConnect(false);
-    }
+        socketRef.current.on("reply", (msg) => {
+          dispatch({
+            type: "UPDATE_PRODUCT_STOCK",
+            payload: {
+              productId: msg.productId,
+              newStock: msg.newStock,
+            },
+          });
+        });
 
-    function onUpdateStock(payload) {
-      console.log(`=== PRODUCT STOCK UPDATED ${JSON.stringify(payload)}`);
-    }
-
-    socket.on("connect", onConnect);
-    socket.on("disconnect", onDisconnect);
-    socket.on("updateStock", onUpdateStock);
-
-    return () => {
-      socket.off("connect", onConnect);
-      socket.off("disconnect", onDisconnect);
-      socket.off("updateStock", onUpdateStock);
+        return () => {
+          socketRef.current.disconnect();
+        };
+      } catch (error) {
+        console.error("Error ocurred when connecting to the socket:", error);
+      }
     };
+
+    handleSocket();
   }, [page, searchTerm]);
 
   const handleLoadMore = async () => {
@@ -56,7 +58,7 @@ const Main = () => {
 
   const handlePurchase = (productId, quantity) => {
     console.log(`Purchasing ${quantity} of product ${productId}`);
-    
+    dispatch(buyProduct({ productId, quantity }));
   };
 
   const filterRenderProducts = (products, searchTerm) => {
