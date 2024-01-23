4a3c5828fb96f1dc5014276acfac3c660f889558
mauudev <mtrigo143@gmail.com>
2024-01-23 02:47:32 -0400

    add prodcut card component

--- frontend/src/components/ProductCard.jsx
+++ frontend/src/components/ProductCard.jsx
@@ -1,12 +1,24 @@
-import * as React from "react";
+import { useState } from "react";
 import Card from "@mui/material/Card";
 import CardActions from "@mui/material/CardActions";
 import CardContent from "@mui/material/CardContent";
 import CardMedia from "@mui/material/CardMedia";
 import Button from "@mui/material/Button";
 import Typography from "@mui/material/Typography";
+import TextField from "@mui/material/TextField";
 
 const ProductCard = ({ product, handlePurchase }) => {
+  const [quantity, setQuantity] = useState(1);
+
+  const handleQuantityChange = (event) => {
+    const value = parseInt(event.target.value, 10);
+    setQuantity(value >= 0 ? value : 0);
+  };
+
+  const handleBuyClick = () => {
+    handlePurchase(product.id, quantity);
+  };
+
   return (
     <Card sx={{ maxWidth: 345 }}>
       <CardMedia sx={{ height: 140 }} image={product.product_image} title={product.product_name} />
@@ -24,7 +36,21 @@ const ProductCard = ({ product, handlePurchase }) => {
         </div>
       </CardContent>
       <CardActions>
-        <Button size="small" variant="outlined" onClick={() => handlePurchase(product.id)}>
+        <TextField
+          id="outlined-number"
+          label="Cantidad"
+          type="number"
+          step="1"
+          min="0"
+          max={product.stock}
+          value={quantity}
+          onChange={handleQuantityChange}
+          InputLabelProps={{
+            shrink: true,
+          }}
+          size="small"
+        />
+        <Button size="small" variant="outlined" onClick={handleBuyClick}>
           Comprar
         </Button>
       </CardActions>
